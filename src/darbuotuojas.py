6d92bac0054192be1e40f08d03aace86de2b08cb
DDRAV <dariusz.dravnel@gmail.com>
2024-06-03 21:07:35 +0300

    add class files

--- src/darbuotuojas.py
+++ src/darbuotuojas.py
@@ -1,11 +1,12 @@
 class Darbuotojas:
-    def __init__(self, vardas: str, pavarde: str, asmens_kodas: int, telefono_numeris: str, elektroninis_pastas: str, alga: str):
+    def __init__(self, vardas: str, pavarde: str, asmens_kodas: int, telefono_numeris: str, elektroninis_pastas: str, alga: str, id: str):
         self.vardas = vardas
         self.pavarde = pavarde
         self.asmens_kodas = asmens_kodas
         self.telefono_numeris = telefono_numeris
         self.elektroninis_pastas = elektroninis_pastas
         self.alga = alga
+        self.id = id
 
     def gauti_informacija(self):
         return {
@@ -14,7 +15,8 @@ class Darbuotojas:
             "Asmens_kodas": self.asmens_kodas,
             "Telefono_numeris": self.telefono_numeris,
             "Elektroninis_pastas": self.elektroninis_pastas,
-            "Alga": self.alga
+            "Alga": self.alga,
+            "ID": self.id
         }
 
     def keisti_informacija(self, vardas=None, pavarde=None, telefono_numeris=None, elektroninis_pastas=None, alga=None):
--- src/transportovadybyninkas.py
+++ src/transportovadybyninkas.py
@@ -5,11 +5,11 @@ class TransportoVadybininkas(Darbuotojas):
         super().__init__(vardas, pavarde, asmens_kodas, telefono_numeris, elektroninis_pastas, alga)
         self.priziurimas_transportas = []
 
-    def perziureti_vadybininko_priziurima_transporta(self):
+    def perziureti_transporta(self):
         print(f"Vadybininkas {self.vardas} {self.pavarde} priziuri {self.priziurimas_transportas} automobilius")
 
-    def priskirti_nauja_automobili(self, automobilis):
+    def priskirti_automobili(self, automobilis):
         self.priziurimas_transportas.append(automobilis)
 
-    def atimti_nauja_automobili(self, automobilis):
+    def atimti_automobili(self, automobilis):
         self.priziurimas_transportas.remove(automobilis)
