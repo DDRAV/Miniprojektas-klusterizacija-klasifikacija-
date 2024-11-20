3bd2c859d38b4ede2f97e0fb16bb443acfe7eebe
DDRAV <dariusz.dravnel@gmail.com>
2024-11-20 13:44:22 +0200

    update

--- Clusterization/clusterisation.py
+++ Clusterization/clusterisation.py
@@ -1,6 +1,10 @@
-import kagglehub
-
+#kodas duomenu rinikio atisuntimui
+#import kagglehub
 # Download latest version
-path = kagglehub.dataset_download("fatihb/coffee-quality-data-cqi")
+#path = kagglehub.dataset_download("fatihb/coffee-quality-data-cqi")
+#print("Path to dataset files:", path)
 
-print("Path to dataset files:", path)
+import pandas as pd
+
+df = pd.read_csv("C:/Users/ddrav/OneDrive - Everwest/Desktop/Projektas/mok/Miniprojektas/Clusterization/df_arabica_clean.csv")
+df.head()
