870eb75a9be842bee52402145c468067aa7ef7ad
DDRAV <dariusz.dravnel@gmail.com>
2024-11-20 14:19:05 +0200

    aa

--- Clusterization/clusterisation.py
+++ Clusterization/clusterisation.py
@@ -1,10 +1,37 @@
-#kodas duomenu rinikio atisuntimui
+#Kode for dataset downloading
 #import kagglehub
 # Download latest version
 #path = kagglehub.dataset_download("fatihb/coffee-quality-data-cqi")
 #print("Path to dataset files:", path)
 
-import pandas as pd
 
-df = pd.read_csv("C:/Users/ddrav/OneDrive - Everwest/Desktop/Projektas/mok/Miniprojektas/Clusterization/df_arabica_clean.csv")
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+path = "C:/Users/ddrav/OneDrive - Everwest/Desktop/Projektas/mok/Miniprojektas/Clusterization/df_arabica_clean.csv"
+
+#1. Get dataset
+
+df = pd.read_csv(path)
 df.head()
+print(df.head())
+
+#2. Handling data
+# Check for duplicate rows
+duplicates = df[df.duplicated()]
+
+# Print duplicates if any
+if not duplicates.empty:
+    print("Duplicate rows found:")
+    print(duplicates)
+else:
+    print("No duplicate rows found.")
+
+# Check for missing values
+print(df.isnull().sum())
+
+# Visualize missing data (optional, if you use seaborn)
+sns.heatmap(df.isnull(), cbar=False, cmap="viridis")
+plt.show()
+
